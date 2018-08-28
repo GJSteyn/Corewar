bea2626b81b0db1b4ef372c8583f2ccfa471eef4
Kmarchan <kmarchan@student.wethinkcode.co.za>
2018-08-28 14:56:12 +0200

    hopefully fixed

---
+++ asm/src/strip_line.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   strip_line.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmarchan <kmarchan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/28 13:50:44 by kmarchan          #+#    #+#             */
+/*   Updated: 2018/08/28 14:34:23 by kmarchan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "core.h"
+
+char *space_space(char *str)
+{
+	char	*new;
+	int		i;
+	int		n;
+
+	i = 0;
+	n = 0;
+	new = (char *)f_memalloc(sizeof(char) * f_strlen(str));
+	while (str[i] != '\0')
+	{
+		while (!f_isspace(str[i])
+			new[n++] = str[i++];
+		if (f_isspace(str[i]) && !f_isspace(str[i - 1]))
+			new[n++] = ' ';
+		else
+			i++;
+	}
+}
+
+t_list	*strip_line(t_list *line)
+{
+	char	*str;
+	t_list	tmp;
+
+	tmp = line;
+	while (tmp)
+	{
+		str = strip_space(t_list *line);
+		tmp = tmp->next;
+	}
+}
---
+++ libcore/include/core.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   core.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmarchan <kmarchan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/28 14:29:36 by kmarchan          #+#    #+#             */
+/*   Updated: 2018/08/28 14:35:08 by kmarchan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef _CORE_LIB_H_
+# define _CORE_LIB_H_
+# include <string.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+size_t					f_strlen(const char *s);
+void					*f_memalloc(size_t size);
+void					f_bzero(void *s, size_t n);
+int						f_isspace(char c);
+
+#endif
