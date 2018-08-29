b8312dea36750bfb3f8df1c8f99604bde00f0f8e
PhilipStubbs <24510543+PhilipStubbs@users.noreply.github.com>
2018-08-29 18:08:12 +0200

    added ldian and bdian, added to makefile and update to core.h, fixed norm errors in a few files

--- libcore/include/core.h
+++ libcore/include/core.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   core.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kmarchan <kmarchan@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pstubbs <pstubbs@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/28 14:29:36 by kmarchan          #+#    #+#             */
-/*   Updated: 2018/08/29 12:40:49 by kmarchan         ###   ########.fr       */
+/*   Updated: 2018/08/29 18:03:09 by pstubbs          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,8 @@ void					*f_memalloc(size_t size);
 void					f_bzero(void *s, size_t n);
 int						f_isspace(char c);
 int						f_isdigit(int c);
+int						f_big_to_little_endian(char big[4]);
+void					f_little_to_big_endian(int little, char big[4]);
+long					f_atol(char *nbr, int *error);
 
 #endif
