9121ed2ee101f70494abad728608de7bb038fc1c
GJSteyn <6443537+GJSteyn@users.noreply.github.com>
2018-08-29 17:57:37 +0200

    Change get_lines to f_get_line. Add function f_dynamicstring, used by f_get_line, and its prototype to core.h

--- libcore/include/core.h
+++ libcore/include/core.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   core.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kmarchan <kmarchan@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gsteyn <gsteyn@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/28 14:29:36 by kmarchan          #+#    #+#             */
-/*   Updated: 2018/08/29 12:40:49 by kmarchan         ###   ########.fr       */
+/*   Updated: 2018/08/29 17:55:10 by gsteyn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,6 @@ void					*f_memalloc(size_t size);
 void					f_bzero(void *s, size_t n);
 int						f_isspace(char c);
 int						f_isdigit(int c);
+char					*f_dynamicstring(char **ori, char *add);
 
 #endif
