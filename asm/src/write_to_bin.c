03dbb3d13aa7b3cad8f3f221db28222cf8eec846
PhilipStubbs <24510543+PhilipStubbs@users.noreply.github.com>
2018-08-30 18:16:51 +0000

    almost finished write_to_bin. WIll test tomorrow

--- asm/src/write_to_bin.c
+++ asm/src/write_to_bin.c
@@ -1,48 +1,60 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   writetobyte.c                                      :+:      :+:    :+:   */
+/*   write_to_bin.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pstubbs <pstubbs@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/30 12:53:58 by pstubbs           #+#    #+#             */
-/*   Updated: 2018/08/30 14:14:21 by pstubbs          ###   ########.fr       */
+/*   Updated: 2018/08/30 18:15:01 by pstubbs          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
 #include <stdio.h> //
 
+void	write_overflow(char bin[MEM_SIZE], int *i, int x, int max)
+{
+	int	tmp;
 
+	tmp = *i;
+	while (x < max)
+	{
+		bin[tmp] = 0; 
+		x++;
+		tmp++;
+	}
+	*i = tmp;
+}
+
+void	write_data(char bin[MEM_SIZE], char *data ,int *i, int max)
+{
+	int	x;
+	int	tmpi;
+
+	x = 0;
+	tmpi = *i;
+	while (data[x])
+	{
+		bin[tmpi] = data[x]; 
+		x++;
+		tmpi++;
+	}
+	*i = tmpi;
+	write_overflow(bin, i, x, max);
+}
 
 void	write_to_bin(char *path, t_header *header)//, t_code *code)
 {
 	char	bin[MEM_SIZE];
-	char	*name;
-	char	*comment;
-	int	t;
-	int	i;
-	int	fd;
+	int		i;
+	int		fd;
 
-	fd = open("test.cor", O_RDWR);
-	name = header->prog_name;
-	comment = header->comment;
+	fd = open(path, O_RDWR);
 	f_little_to_big_endian(header->magic, bin);
 	i = 4;
-	while (*name)
-	{
-		f_little_to_big_endian(*name, bin + i);
-		// printf("[%c]", (*name));
-		name++;
-		i += 4;
-	}
-	while (*comment)
-	{
-		f_little_to_big_endian(*comment, bin + i);
-		comment++;
-		i += 4;
-	}
-	// printf("[%c]\n",bin[4] );
+	write_data(bin, header->prog_name, &i, PROG_NAME_LENGTH + 1);
+	write_data(bin, header->comment, &i , COMMENT_LENGTH +1);
 	write(fd, bin, i);
 }
 
@@ -51,10 +63,10 @@ int main()
 	t_header *header;
 
 	header = (t_header*)malloc(sizeof(t_header));
-	header->magic = 6548932;
+	header->magic = 59;
 	memcpy(header->prog_name, "IVY\0", 3);
 	memcpy(header->comment, "THIS PROGRAMS NAME IS IVY, IN REF TO IVY TOWER\0", 46);
-	write_to_bin(NULL, header);
+	write_to_bin("test.cor", header);
 	return (1);
 }
 
