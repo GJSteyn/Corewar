764b2bb27427e19276260dab697594ef25cb1431
Kmarchan <kmarchan@student.wethinkcode.co.za>
2018-08-30 10:47:43 +0200

    stripper working

--- libcore/include/core.h
+++ libcore/include/core.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   core.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pstubbs <pstubbs@student.42.fr>            +#+  +:+       +#+        */
+/*   By: kmarchan <kmarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/28 14:29:36 by kmarchan          #+#    #+#             */
-/*   Updated: 2018/08/29 18:03:09 by pstubbs          ###   ########.fr       */
+/*   Updated: 2018/08/30 10:42:55 by kmarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,13 @@
 # include <string.h>
 # include <unistd.h>
 # include <stdlib.h>
-# include "list.h"
+// # include "list.h"
 
 size_t					f_strlen(const char *s);
 void					*f_memalloc(size_t size);
 void					f_bzero(void *s, size_t n);
 int						f_isspace(char c);
+int						f_isspace_notnewl(char c);
 int						f_isdigit(int c);
 int						f_big_to_little_endian(char big[4]);
 void					f_little_to_big_endian(int little, char big[4]);
