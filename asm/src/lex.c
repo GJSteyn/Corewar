4c5c961110d0fa08759cdd9f43670ceaccbe00c2
GJSteyn <36443537+GJSteyn@users.noreply.github.com>
2018-08-30 19:02:15 +0200

    Secondary lex code backup

--- asm/src/lex.c
+++ asm/src/lex.c
@@ -6,7 +6,7 @@
 /*   By: gsteyn <gsteyn@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/29 18:05:12 by gsteyn            #+#    #+#             */
-/*   Updated: 2018/08/29 19:19:41 by gsteyn           ###   ########.fr       */
+/*   Updated: 2018/08/30 18:55:17 by gsteyn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,20 +37,164 @@ static bool		f_strmatch(const char *str, char *match)
 	return (true);
 }
 
-static void		append_name(t_list *list)
+static int		f_intlen(int n)
 {
-	char		*data;
+	int		ret;
 
-	data = f_strdup(NAME_CMD_STRING);
-	list_append(list, data);
+	ret = 0;
+	if (n == 0)
+		return (1);
+	while (n)
+	{
+		n /= 10;
+		ret++;
+	}
+	return (ret);
 }
 
-static void		append_comment(t_list *list)
+static char		*get_text(char *str)
 {
-	char		*data;
+	char			*tmp;
+	char			*ret;
 
-	data = f_strdup(COMMENT_CMD_STRING);
-	list_append(list, data);
+	tmp = str + 1;
+	while (*tmp && *tmp != '"')
+		tmp++;
+	ret = f_strsub(str, 1, tmp - str - 2);
+	return (ret);
+}
+
+static bool		is_label(char *str)
+{
+	while (f_strchr(LABEL_CHARS, *str))
+		str++;
+	if (*str == ':')
+		return (true);
+	return (false);
+}
+
+static void		add_newline(t_list *list, size_t line)
+{
+	struct s_token	*token;
+
+	token = (struct s_token*)f_memalloc(sizeof(struct s_token));
+	token->type = eol;
+	token->line = line;
+	list_append(list, token);
+}
+
+static void		add_name(t_list *list, size_t line)
+{
+	struct s_token	*token;
+
+	token = (struct s_token*)f_memalloc(sizeof(struct s_token));
+	token->type = keyword;
+	token->value.keyword = name;
+	token->line = line;
+	list_append(list, token);
+}
+
+static void		add_comment(t_list *list)
+{
+	struct s_token	*token;
+
+	token = (struct s_token*)f_memalloc(sizeof(struct s_token));
+	token->type = keyword;
+	token->value.keyword = comment;
+	token->line = line;
+	list_append(list, token);
+}
+
+static void		add_text(t_list *list, char **str, size_t line)
+{
+	struct s_token	*token;
+	char			*string;
+
+	token = (struct s_token*)f_memalloc(sizeof(struct s_token));
+	string = get_text(str);
+	*str += f_strlen(string) + 2;
+	token->type = text;
+	token->value.text = string;
+	token->line = line;
+	list_append(list, token);
+}
+
+static void		add_number(t_list *list, char **str, size_t line)
+{
+	struct s_token	*token;
+
+	token = (struct s_token*)f_memalloc(sizeof(struct s_token));
+	token->type = number;
+	token->value.number = (int)f_atol(*str);
+	token->line = line;
+	*str += f_intlen(token->value.number);
+	list_append(list, token);
+}
+
+static void		add_arg(t_list *list, char **str, size_t line)
+{
+	struct s_token	*token;
+
+	token = (struct s_token*)f_memalloc(sizeof(struct s_token));
+	token->type = arg;
+	if (*str + 1 == ':')
+	{
+		token->value.arg = label;
+		*str += 2;
+	}
+	else
+	{
+		token->value.arg = direct;
+		*str += 1;
+	}
+	token->line = line;
+	list_append(list, token);
+}
+
+static void		add_label(t_list *list, char **str, size_t line)
+{
+	struct s_token	*token;
+	char			*tmp;
+	char			*label;
+
+	token = (struct s_token*)f_memalloc(sizeof(struct s_token));
+	token->type = label;
+	tmp = *str;
+	while (*tmp != ':')
+		tmp++;
+	label = f_strsub(*str, 0, tmp - *str - 1);
+	token->value.text = label;
+	*str += tmp - *str + 1;
+	token->line = line;
+	list_append(list, token);
+}
+
+static void		add_token(char **str, size_t *line, t_list *list)
+{
+	if (**str == '\n')
+	{
+		add_newline(list, *line);
+		(*line)++;
+		(*str)++;
+	}
+	else if (f_strmatch(*str, NAME_CMD_STRING))
+	{
+		add_name(list, *line)
+		*str += f_strlen(NAME_CMD_STRING);
+	}
+	else if (f_strmatch(*str, COMMENT_CMD_STRING))
+	{
+		add_comment(list, *line);
+		*str += f_strlen(COMMENT_CMD_STRING);
+	}
+	else if (**str == '"')
+		add_text(list, str, *line);				// Should str be incremented here?			(rather than in the function that gets called here)
+	else if (f_isdigit(**str) || **str == '-')
+		add_number(list, str, *line);			// Should str be incremented here?
+	else if (**str == DIRECT_CHAR)
+		add_arg(list, str, *line);				// Should str be incremented here?
+	else if (is_label(*str))
+		add_label(list, str, *line);			// Should str be incremented here?
 }
 
 t_list			lex(char *clean_line)
@@ -64,11 +208,9 @@ t_list			lex(char *clean_line)
 	it = line;
 	while (*it)
 	{
-		if (f_strmatch(it, NAME_CMD_STRING))
-			append_name(ret);
-		else if (f_strmatch(it, COMMENT_CMD_STRING))
-			append_comment(ret);
-		else if (*it == '"')
-			append_text(ret)
+		if (f_isspace(*it) && *it != '\n')		// Should f_isspace be checking for '\n'?
+			it++;
+		else
+			add_token(&it, &line, ret);
 	}
 }
