564be8ca3600fecba543e166204fc23da77f2a82
PhilipStubbs <24510543+PhilipStubbs@users.noreply.github.com>
2018-08-31 13:41:14 +0200

    update

--- asm/src/write_to_bin.c
+++ asm/src/write_to_bin.c
@@ -6,7 +6,7 @@
 /*   By: pstubbs <pstubbs@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/30 12:53:58 by pstubbs           #+#    #+#             */
-/*   Updated: 2018/08/30 18:15:01 by pstubbs          ###   ########.fr       */
+/*   Updated: 2018/08/31 11:32:33 by pstubbs          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	write_overflow(char bin[MEM_SIZE], int *i, int x, int max)
 	int	tmp;
 
 	tmp = *i;
-	while (x < max)
+	while (x <= max)
 	{
 		bin[tmp] = 0; 
 		x++;
@@ -40,7 +40,7 @@ void	write_data(char bin[MEM_SIZE], char *data ,int *i, int max)
 		x++;
 		tmpi++;
 	}
-	*i = tmpi;
+	*i = tmpi + 1;
 	write_overflow(bin, i, x, max);
 }
 
@@ -54,8 +54,11 @@ void	write_to_bin(char *path, t_header *header)//, t_code *code)
 	f_little_to_big_endian(header->magic, bin);
 	i = 4;
 	write_data(bin, header->prog_name, &i, PROG_NAME_LENGTH + 1);
-	write_data(bin, header->comment, &i , COMMENT_LENGTH +1);
-	write(fd, bin, i);
+	i++;
+	f_little_to_big_endian(header->prog_size, bin + i);
+	i += 4;
+	write_data(bin, header->comment, &i, COMMENT_LENGTH + 1);
+	write(fd, bin, i + 1);
 }
 
 int main()
@@ -63,7 +66,8 @@ int main()
 	t_header *header;
 
 	header = (t_header*)malloc(sizeof(t_header));
-	header->magic = 59;
+	header->magic = COREWAR_EXEC_MAGIC;
+	header->prog_size = 49;
 	memcpy(header->prog_name, "IVY\0", 3);
 	memcpy(header->comment, "THIS PROGRAMS NAME IS IVY, IN REF TO IVY TOWER\0", 46);
 	write_to_bin("test.cor", header);
