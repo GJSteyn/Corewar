5ed430371acf9529c976d6b9a3d680abf5e101bd
GJSteyn <36443537+GJSteyn@users.noreply.github.com>
2018-08-31 17:15:25 +0200

    Comment out static f_strnew for now

--- asm/src/f_get_line.c
+++ asm/src/f_get_line.c
@@ -6,7 +6,7 @@
 /*   By: gsteyn <gsteyn@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/29 16:23:59 by gsteyn            #+#    #+#             */
-/*   Updated: 2018/08/29 17:52:46 by gsteyn           ###   ########.fr       */
+/*   Updated: 2018/08/31 15:43:02 by gsteyn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,19 @@
 #include <fcntl.h>
 #include <errno.h>
 
-static char		*f_strnew(size_t size)
-{
-	size_t	i;
-	char	*ret;
+// static char		*f_strnew(size_t size)
+// {
+// 	size_t	i;
+// 	char	*ret;
 
-	i = 0;
-	ret = (char*)f_memalloc(size + 1);
-	if (!ret)
-		return (NULL);
-	while (i < size)
-		ret[i++] = '\0';
-	return (ret);
-}
+// 	i = 0;
+// 	ret = (char*)f_memalloc(size + 1);
+// 	if (!ret)
+// 		return (NULL);
+// 	while (i < size)
+// 		ret[i++] = '\0';
+// 	return (ret);
+// }
 
 char			*f_get_line(char *path)
 {
