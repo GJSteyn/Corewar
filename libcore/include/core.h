5e16f24cff0715e7a7a112cec90365853e77f1ed
GJSteyn <36443537+GJSteyn@users.noreply.github.com>
2018-08-31 17:17:48 +0200

    Add f_atol reference in core.h and fix ft_strchr typo. Also added missing references in libcore Makefile

--- libcore/include/core.h
+++ libcore/include/core.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   core.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wseegers <wseegers@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gsteyn <gsteyn@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/28 14:29:36 by kmarchan          #+#    #+#             */
-/*   Updated: 2018/08/31 09:04:25 by wseegers         ###   ########.fr       */
+/*   Updated: 2018/08/31 15:07:14 by gsteyn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 # include <stdlib.h>
 # include "../../liblist/include/list.h"
 
+long					f_atol(char *nbr, int *error);
 size_t					f_strlen(const char *s);
 void					*f_memalloc(size_t size);
 void					f_bzero(void *s, size_t n);
@@ -25,7 +26,7 @@ int						f_isdigit(int c);
 char					*f_dynamicstring(char **ori, char *add);
 char					*f_strsub(char const *s, unsigned int st, size_t l);
 char					*f_strnew(size_t size);
-char					*ft_strchr(const char *s, int c);
+char					*f_strchr(const char *s, int c);
 int						f_strcmp(const char *s1, const char *s2);
 char					*f_strncpy(char *dest, const char *src, size_t n);
 
