5d471615de802b9da0a398b2367674e20ada9994
GJSteyn <36443537+GJSteyn@users.noreply.github.com>
2018-09-05 04:44:11 +0200

    Testing parse_instructions phase

--- asm/src/main.c
+++ asm/src/main.c
@@ -6,7 +6,7 @@
 /*   By: gsteyn <gsteyn@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/30 10:53:19 by kmarchan          #+#    #+#             */
-/*   Updated: 2018/09/04 17:41:42 by gsteyn           ###   ########.fr       */
+/*   Updated: 2018/09/05 04:30:02 by gsteyn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,17 @@ int		main(int argc, char **argv)
 	char		*no_com;
 	t_list		*token_list;
 	t_header	*header;
-	t_list		*instructions;
+	// t_list		*instructions;
 
 	if (argc == 2)
 	{
 		no_sp = get_line(argv[1]);
 		no_com = strip_line(no_sp);
+		printf("%s", no_sp);
 		puts("debug");
 		token_list = lex(no_com);
 		header = parse_header(token_list);
-		instructions = parse_instructions(token_list);
+		// instructions = parse_instructions(token_list);
 		printf("name: %s\n", header->prog_name);
 		printf("comment: %s\n", header->comment);
 		free(no_com);
