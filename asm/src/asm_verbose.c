59dab0bbe1858f72b3b98b8e0553a77a19b89983
GJSteyn <36443537+GJSteyn@users.noreply.github.com>
2018-09-13 16:08:31 +0200

    Change handling of lists in asm_verbose so they aren't deleted while iterating

--- asm/src/asm_verbose.c
+++ asm/src/asm_verbose.c
@@ -6,13 +6,14 @@
 /*   By: gsteyn <gsteyn@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/12 12:15:52 by gsteyn            #+#    #+#             */
-/*   Updated: 2018/09/12 16:15:06 by gsteyn           ###   ########.fr       */
+/*   Updated: 2018/09/13 16:07:00 by gsteyn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parse.h"
 #include "f_print.h"
 #include "s_instruction.h"
+#include "list.h"
 
 typedef struct s_instruction	t_instruction;
 char		*g_types[9] =
@@ -81,33 +82,35 @@ static void		print_value(t_token *t)
 
 void			print_instructions(t_header *header, t_instr_list *instr_list)
 {
-	t_instruction	*instr;
+	t_list_node		*instr;
 	int				i;
 
 	f_printf("HEADER\n	NAME: %7s\n	COMMENT: %7s\n",
 										header->prog_name, header->comment);
-	while ((instr = (t_instruction*)list_pop(instr_list, 0)))
+	instr = instr_list->head;
+	while (instr)
 	{
-		f_printf("OP: %s\n", g_ops[instr->op]);
+		f_printf("OP: %s\n", g_ops[((t_instruction*)instr->data)->op]);
 		i = -1;
-		while (++i < g_op_tab[instr->op - 1].argc)
+		while (++i < g_op_tab[((t_instruction*)instr->data)->op - 1].argc)
 			f_printf("	ARG: type = %-8s | value = %-7d\n",
-					g_args[instr->arg_type[i]], instr->arg_value[i]);
-		instruction_destroy(instr);
+					g_args[((t_instruction*)instr->data)->arg_type[i]], ((t_instruction*)instr->data)->arg_value[i]);
+		instr = instr->next;
 	}
 }
 
 void			print_tokens(t_token_list *token_list)
 {
-	t_token		*t;
-	size_t		i;
+	t_list_node		*t;
+	size_t			i;
 
+	t = token_list->head;
 	i = 0;
-	while ((t = DEQUE_TOKEN(token_list)))
+	while (t)
 	{
-		f_printf("%zu: %4zu -> %-10s|", t->line, i++, g_types[t->type]);
-		print_value(t);
+		f_printf("%zu: %4zu -> %-10s|", ((t_token*)t->data)->line, i++, g_types[((t_token*)t->data)->type]);
+		print_value((t_token*)t->data);
 		f_printf("\n");
-		token_destroy(&t);
+		t = t->next;
 	}
 }
