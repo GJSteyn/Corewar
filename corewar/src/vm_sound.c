53575c218bc481ca39fa749ffed2161a15f255ed
GJSteyn <36443537+GJSteyn@users.noreply.github.com>
2018-09-27 10:30:51 +0200

    Add absolute path to sound file

--- corewar/src/vm_sound.c
+++ corewar/src/vm_sound.c
@@ -6,7 +6,7 @@
 /*   By: gsteyn <gsteyn@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/26 13:29:38 by gsteyn            #+#    #+#             */
-/*   Updated: 2018/09/26 14:43:09 by gsteyn           ###   ########.fr       */
+/*   Updated: 2018/09/27 10:19:36 by gsteyn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ void		vm_sound(void)
 	g_env.vis_env.sound_pid = fork();
 	if (!g_env.vis_env.sound_pid)
 	{
-		execlp("afplay", "afplay", "../sound/Corewar_VM_Sound.mov", NULL);
+		// execlp("afplay", "afplay", "../sound/Corewar_VM_Sound.mov", NULL);
+		execlp("afplay", "afplay", SOUND, NULL);
 		exit(0);
 	}
 }
